744cc24d20eaf891207a76f80af22b92df0f7636
vsk <kandhanvsk@gmail.com>
2023-10-09 22:09:17 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Resume-Builder this is the simple prototype for my upcoming project
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{Component} from 'react'
+import ReacDOM from 'react-dom'
+import './index.css'
+const root=document.getElementById('root')
+class Form extends Component{
+    state={
+        firstname:'',
+        role:'',
+        skills:{
+            html:false,
+            css:false,
+            js:false
+        },
+        
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    }
+handleChange=(e)=>{
+    const {name,value,type,checked}=e.target
+    console.log(checked,'check message');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    if(type==='checkbox'){
+    this.setState({
+        skills:{...this.state.skills,[name]:checked}})
+
+
+
+
+    }
+    else{
+        this.setState({[name]:value})
+    }
+
+}
+render(){
+    let skills=this.state.skills
+    let skillList=[]
+    for(const k in skills){
+        if(skills[k]){
+skillList.push(k)
+        }
+    }
+   let skillDiv= skillList.map((e)=><li key={e}>{e}</li>)
+
+    console.log(skillDiv);
+
+    return(
+        <div className='main'>
+        <div className='form'>
+            <div>
+        <label htmlFor='fname'>Your Name Please?</label>
+        <input type='text' id='fname' name='firstname'onChange={this.handleChange}  value={this.state.firstname} /></div>
+        <div>
+        <label htmlFor='role'>What's Your Role</label>
+        <input type='text' id='role' name='role'onChange={this.handleChange}  value={this.state.role} /></div>
+       <div>
+        <label htmlFor='html'>HTML</label>
+        <input type='checkbox' id='html' name='html'onChange={this.handleChange}  value={this.state.skills['html']} /></div>
+        <div>
+        <label htmlFor='css'>CSS</label>
+        
+        <input type='checkbox' id='css' name='css'onChange={this.handleChange}  value={this.state.skills['css']} /></div>
+       <div>
+        <label htmlFor='js'>JS</label>
+        
+        <input type='checkbox' id='js' name='js'onChange={this.handleChange}  value={this.state.skills['js']} />
+        </div></div>
+       <div className='card'>
+        <h1>Resume</h1>
+        <h3>Myself</h3>
+        <h2>{this.state.firstname}, iam a <strong> {this.state.role}</strong></h2>
+        <h3>My Skills:</h3>
+        <ul>{skillDiv}</ul>
+        </div>
+
+        </div>
+    )
+}
+
+}
+ReacDOM.render(<Form/>,root)
